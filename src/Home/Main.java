fb65d3154f5867d7c98e3f0575646613b768e510
BuildTools <unconfigured@null.spigotmc.org>
2020-08-26 16:54:09 -0400

    Second Commit 26/08/2020: Added drag window, button to close and iconify.

--- src/Home/Main.java
+++ src/Home/Main.java
@@ -1,21 +1,48 @@
 package Home;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
-
+    static Stage window;
+    private static double xOffset = 0;
+    private static double yOffset = 0;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/sample2.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        window = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/MainScreenVersion3.fxml"));
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = primaryStage.getX() - event.getScreenX();
+                yOffset = primaryStage.getY() - event.getScreenY();
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() + xOffset);
+                primaryStage.setY(event.getScreenY() + yOffset);
+            }
+        });
+
+
+        window.setTitle("Hello World");
+        window.initStyle(StageStyle.UNDECORATED);
+        window.setScene(new Scene(root));
+        window.show();
+    }
+
+    public static void minimizeWindow(){
+        window.setIconified(true);
     }
 
 
--- src/Home/controllers/Controller.java
+++ src/Home/controllers/Controller.java
@@ -1,4 +1,70 @@
 package Home.controllers;
 
-public class Controller {
+import Home.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
+
+    private GregorianCalendar gcal = new GregorianCalendar();
+
+
+    @FXML
+    private Button btnAction;
+    @FXML
+    private Label lblDayOfWeek;
+
+    @FXML
+    public void handleBtnPrintDate(){
+    }
+    @FXML
+    public void handleBtnExit(){
+        Platform.exit();
+    }
+    @FXML
+    public void handleBtnMinimize(){
+        Main.minimizeWindow();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String strDate;
+        strDate = parseDayOfWeek(gcal.get(Calendar.DAY_OF_WEEK)) + " " + gcal.get(Calendar.DAY_OF_MONTH);
+        lblDayOfWeek.setText(strDate);
+
+    }
+
+
+
+
+
+    public String parseDayOfWeek(int numberDay){
+        String dayOfWeekString;
+        switch (numberDay){
+            case 1: dayOfWeekString = "Sunday"; break;
+            case 2: dayOfWeekString = "Monday"; break;
+            case 3: dayOfWeekString = "Tuesday"; break;
+            case 4: dayOfWeekString = "Wednesday"; break;
+            case 5: dayOfWeekString = "Thrusday"; break;
+            case 6: dayOfWeekString = "Friday"; break;
+            case 7: dayOfWeekString = "Saturday"; break;
+            default: dayOfWeekString="Error"; break;
+        }
+        return dayOfWeekString;
+    }
+
+
+
+
+
+
 }
